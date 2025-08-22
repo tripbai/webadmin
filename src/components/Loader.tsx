016effcdde46899bca2e91274a9d9c349001495f
kenjiefx <terradoflex@gmail.com>
2025-08-22 21:51:24 +0800

    refactor-2025-08-22

--- src/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Loader() {
-  return (
-    <div className="h-screen size-full flex items-center justify-center">
-      <div className="--page-loading-animation"></div>
-    </div>
-  );
-}
--- src/providers/StoreProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { store } from '../state/store'
-import { Provider } from 'react-redux'
-
-export const StoreProvider = ({ children }: { children: React.ReactNode } ) => {
-    return <Provider store={store}>{children}</Provider>
-}
