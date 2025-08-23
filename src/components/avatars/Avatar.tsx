4cf5eb1314df664047822d8af7d500feec93e190
kenjiefx <terradoflex@gmail.com>
2025-08-23 12:00:36 +0800

    feat: avatar large size option

--- src/components/avatars/Avatar.tsx
+++ src/components/avatars/Avatar.tsx
@@ -6,16 +6,23 @@ type Props = {
   src: string | null;
   alt: string;
   fallback: JSX.Element;
+  size?: "small" | "large";
 };
 
-export default function Avatar({ src, alt, fallback }: Props) {
+export default function Avatar({ src, alt, fallback, size = "small" }: Props) {
+  const getWidthAndHeightBasedOnSize = () => {
+    if (size === "small") {
+      return "w-10 h-10";
+    }
+    return "w-40 h-40";
+  };
   return (
     <div className="relative inline-block">
       {src ? (
         <img
           src={src}
           alt={alt}
-          className="w-10 h-10 rounded-full border border-gray-300"
+          className={`${getWidthAndHeightBasedOnSize()} rounded-full border border-gray-300`}
         />
       ) : (
         fallback
--- src/components/avatars/NameInitialsAvatar.tsx
+++ src/components/avatars/NameInitialsAvatar.tsx
@@ -2,12 +2,28 @@
 
 type Props = {
   firstName: string;
+  size?: "small" | "large";
 };
 
-export default function NameInitialsAvatar({ firstName }: Props) {
+export default function NameInitialsAvatar({
+  firstName,
+  size = "small",
+}: Props) {
   const getInitial = (name: string) => {
     if (!name) return "?";
     return name.trim().charAt(0).toUpperCase();
+  };
+  const getWidthAndHeightBasedOnSize = () => {
+    if (size === "small") {
+      return "w-10 h-10";
+    }
+    return "w-40 h-40";
+  };
+  const getTextSizeBasedOnSize = () => {
+    if (size === "small") {
+      return "text-md";
+    }
+    return "text-2xl";
   };
   const initial = getInitial(firstName);
   const colors = [
@@ -23,9 +39,11 @@ export default function NameInitialsAvatar({ firstName }: Props) {
   const bgColor = colors[colorIndex];
   return (
     <div
-      className={`w-10 h-10 rounded-full flex items-center justify-center ${bgColor}`}
+      className={`${getWidthAndHeightBasedOnSize()} rounded-full flex items-center justify-center ${bgColor}`}
     >
-      <span className="text-white font-bold">{initial}</span>
+      <span className={`text-white font-bold ${getTextSizeBasedOnSize()}`}>
+        {initial}
+      </span>
     </div>
   );
 }
